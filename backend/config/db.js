fcf4c83a970625b7ae8cbe91796af89d9c0e41db
Kunal <kunallondhex@gmail.com>
2024-08-27 16:15:45 +0530

    Schema Created and Encryption done.

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ import colors from 'colors';
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB connected: ${conn.connection.host}`.bgYellow.bold.green);
+        console.log(`MongoDB connected: ${conn.connection.host}`.bgYellow.bold);
     } catch (err) {
         console.error(`Error: ${err.message}`);
         process.exit(1); //bad request
