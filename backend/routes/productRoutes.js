1550a856d65a14007d3601c0bfbaae0069c1e688
Kunal <kunallondhex@gmail.com>
2024-08-29 18:42:30 +0530

    Bakend seeder created and Routes added done.

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import Product from '../models/productModel.js';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    const products = await Product.find({}); // to find everything
+    res.json(products);
+});
+
+router.get('/:id', async (req , res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.json(product);
+    } catch (err) {
+        console.error(`message: ${err.message}`)
+    }
+});
+
+export default router;
