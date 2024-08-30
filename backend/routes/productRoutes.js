a50829e92fab97a2bfc9245aa361b954a56efc59
KUNALL0ndhe <kunallondhex@outlook.com>
2024-08-30 18:17:45 +0530

    Express-async-handler installed and error handler taken care.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,21 @@
 import express from 'express';
 import Product from '../models/productModel.js';
+import asyncHandler from 'express-async-handler';
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    const products = await Product.find({}); // to find everything in product
-    res.json(products);
-});
+router.get('/', 
+    asyncHandler(async (req, res) => {
+        const products = await Product.find({}); // to find everything in product
+        res.json(products);
+    })
+);
 
-router.get('/:id', async (req , res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        res.json(product);
-    } catch (err) {
-        console.error(`message: ${err.message}`)
-    }
-});
+router.get('/:id', 
+    asyncHandler(async (req , res) => {
+            const product = await Product.findById(req.params.id);
+            res.json(product);
+      })
+);
 
 export default router;
