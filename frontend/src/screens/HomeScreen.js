a3e8660df7cfda0b087fe5557498f88755c8909d
KUNALL0ndhe <kunallondhex@outlook.com>
2024-09-03 14:31:10 +0530

    First reducer, Home screen added.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,32 +1,37 @@
 import { Grid, Heading } from "@chakra-ui/react";
 import ProductCards from "../components/ProductCard";
-import axios from 'axios';
-import { useState, useEffect } from "react";
+import { listProducts } from "../actions/productActions";
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([]);
+
+    const dispatch = useDispatch();
+
+    const productList = useSelector((state) => state.productList);
+    const { loading, products, error } = productList
     
     useEffect (() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get(`/api/products`);
-            setProducts(data);
-        };
-
-        fetchProducts();
-    }, []);
+        dispatch(listProducts());       
+    }, [dispatch]);
     return (
         <>
         <Heading as='h2' mb='8' fontSize='xl'>
             Latest Products
         </Heading>
 
+        { loading ? (
+            <p>Loading ...</p>
+        ) : error ?(
+             <p> {error} ...</p>
+        ) : (
         <Grid templateColumns='1fr 1fr 1fr 1fr' gap='8' >
-            {products.map((prod) => {
-                return  (
-                    <ProductCards product={prod} key={prod._id} />
-                )
-            })}
-        </Grid>
+        {products.map((prod) => {
+            return  (
+                <ProductCards product={prod} key={prod._id} />
+            )
+        })}
+    </Grid>)}
         </>
     )
 };
